861a88978cc1ebf280f0d03ef16639ac0ac10602
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-05-15 21:32:51 +0300

    feat: add IllegalArgumentException method handler, change annotation

--- src/main/java/ru/clevertec/ecl/controller/CustomExceptionHandler.java
+++ src/main/java/ru/clevertec/ecl/controller/CustomExceptionHandler.java
@@ -4,8 +4,8 @@ import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -16,13 +16,25 @@ import java.util.NoSuchElementException;
 /**
  * Handle exceptions
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({MethodArgumentTypeMismatchException.class})
     public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
             MethodArgumentTypeMismatchException ex, WebRequest request) {
         String errorMessage = ex.getName() + " should be of type " + ex.getRequiredType().getName();
+
+        ErrorResponse apiError = new ErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                errorMessage + " (" + ex.getLocalizedMessage() + ")",
+                request.getDescription(false));
+        return new ResponseEntity<>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<Object> handleIllegalArgumentException(
+            IllegalArgumentException ex, WebRequest request) {
+        String errorMessage = ex.getMessage();
 
         ErrorResponse apiError = new ErrorResponse(
                 HttpStatus.BAD_REQUEST,
